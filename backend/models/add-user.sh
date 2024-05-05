51d4e1b6396dd47d8c246f464e3ab3ee95d3871a
LukaLmelias <lmeliasluke@gmail.com>
2024-05-05 11:49:06 +0200

    adopted scripts from https://github.com/b01/docker-images/tree/main/alpine-mongodb as a work-around to getting mongodb on alpine distro

---
+++ backend/models/add-user.sh
@@ -0,0 +1,66 @@
+
+#!/bin/sh -e
+
+
+
+#####################################################################
+############### script adopted from: https://github.com/b01/docker-images/blob/main/alpine-mongodb/add-user.sh
+#####################################################################
+# Get root user
+if [ -n "${MONGO_USER}" ]; then
+    mongoUser="${MONGO_USER}"
+else
+    mongoUser="root"
+fi
+
+# Get a password.
+if [ -n "${MONGO_PASS}" ]; then
+    mongoPass="${MONGO_PASS}"
+else
+    mongoPass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+fi
+
+# Get admin database.
+if [ -n "${MONGO_ADMIN_DB}" ]; then
+    mongoAdminDb="${MONGO_ADMIN_DB}"
+else
+    mongoAdminDb="admin"
+fi
+
+# Generate a script to add a user to Mongo DB.
+x="use admin;\n"
+x="${x}db.createUser({\n"
+x="${x}    \"user\": \"${mongoUser}\",\n"
+x="${x}    \"pwd\": \"${mongoPass}\",\n"
+x="${x}    \"roles\": [\n"
+x="${x}        { \"role\": \"root\", \"db\": \"${mongoAdminDb}\" },\n"
+x="${x}        { \"role\": \"userAdminAnyDatabase\", \"db\": \"${mongoAdminDb}\" },\n"
+x="${x}        { \"role\": \"restore\", \"db\": \"${mongoAdminDb}\"}\n"
+x="${x}    ]\n"
+x="${x}});"
+
+# Save to a file.
+printf "Generating script to add a root user.\n"
+printf "${x}" > $ADD_USER_FILE
+
+if [ -f "${ADD_USER_FILE}" ]; then
+    # Start Mongo DB in the background.
+    mongod --fork --port 27017 --dbpath "${MONGO_DKR_DATA_DIR}" --logpath "${MONGO_DKR_LOG_DIR}"/mongod.log --pidfilepath "${MONGO_RUN_PID_FILE}"
+    # ADD User.
+    mongo < $ADD_USER_FILE
+    # Shutdown Mongo DB server.
+    mongod --shutdown --dbpath "${MONGO_DKR_DATA_DIR}"
+    # Remove the add user script.
+    rm $ADD_USER_FILE
+fi
+
+# Display the generated account info.
+y="Here is a Mongo DB generated account:\n"
+y="This info will ONLY be shown ONCE, PLEASE write it down.\n"
+y="${y}admin database: ${mongoAdminDb}\n"
+y="${y}          user: ${mongoUser}\n"
+y="${y}          pass: ${mongoPass}\n\n"
+y="${y}How to use to login:\n"
+y="${y}\tdocker exec -it alpine-mongodb mongo -u ${mongoUser} -p ${mongoPass} --authenticationDatabase ${mongoAdminDb}\n\n"
+
+printf "${y}"
---
+++ backend/models/start-mongod.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#####################################################################
+############### script adopted from: https://github.com/b01/docker-images/blob/main/alpine-mongodb/start-mongod.sh
+#####################################################################
+
+shutd () {
+    echo "Shutting down MongoDB..." >&2
+    mongod --config /etc/mongod.conf --shutdown
+    exit
+}
+trap 'shutd' SIGTERM
+
+printf "running as user " && whoami
+
+echo "start-mongod.sh  Pid=$$"
+
+# Run script to add a root user with a generated password.
+echo "add user file: ${ADD_USER_CMD}"
+if [ -f "${ADD_USER_CMD}" ]; then
+    printf "running ${ADD_USER_CMD} script\n\n"
+    source $ADD_USER_CMD
+    rm -v $ADD_USER_CMD
+fi
+
+printf "starting mongod, all interaction with the server will output below:\n" && whoami
+
+# Start the mongo server in the foreground to keep the container running.
+exec mongod --config /etc/mongod.conf
